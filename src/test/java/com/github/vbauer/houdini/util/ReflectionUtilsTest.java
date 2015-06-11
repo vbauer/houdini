cdb52cc0147da7718ee77f02d231a70d4669fe37
Vladislav Bauer <bauer.vlad@gmail.com>
2015-06-12 03:06:57 +0600

    Add unit tests for ReflectionUtils

--- src/test/java/com/github/vbauer/houdini/util/ReflectionUtilsTest.java
+++ src/test/java/com/github/vbauer/houdini/util/ReflectionUtilsTest.java
@@ -4,6 +4,9 @@ import com.github.vbauer.houdini.core.BasicTest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.UndeclaredThrowableException;
+
 /**
  * @author Vladislav Bauer 
  */
@@ -26,6 +29,53 @@ public class ReflectionUtilsTest extends BasicTest {
     @Test
     public void testConstructorContract() throws Exception {
         checkUtilConstructorContract(ReflectionUtils.class);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testHandleReflectionException1() {
+        ReflectionUtils.handleReflectionException(new NoSuchMethodException());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testHandleReflectionException2() {
+        ReflectionUtils.handleReflectionException(new IllegalAccessException());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testHandleReflectionException3() {
+        ReflectionUtils.handleReflectionException(new RuntimeException());
+    }
+
+    @Test(expected = UndeclaredThrowableException.class)
+    public void testHandleReflectionException4() {
+        ReflectionUtils.handleReflectionException(new Exception());
+    }
+
+    @Test(expected = Error.class)
+    public void testHandleReflectionException5() {
+        ReflectionUtils.handleReflectionException(new InvocationTargetException(new Error()));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testRethrowRuntimeException1() {
+        ReflectionUtils.rethrowRuntimeException(new RuntimeException());
+    }
+
+    @Test(expected = Error.class)
+    public void testRethrowRuntimeException2() {
+        ReflectionUtils.rethrowRuntimeException(new Error());
+    }
+
+    @Test(expected = UndeclaredThrowableException.class)
+    public void testRethrowRuntimeException3() {
+        ReflectionUtils.rethrowRuntimeException(new Exception());
+    }
+
+    @Test(expected = Error.class)
+    public void testHandleInvocationTargetException() {
+        ReflectionUtils.handleInvocationTargetException(
+            new InvocationTargetException(new Error())
+        );
     }
 
 }
